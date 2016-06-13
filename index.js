829f98c798e1b366222aca62f9c0f0ea385e7b94
Anirudh <anirudhx5@gmail.com>
2016-06-13 15:11:00 +0100

    welcome message with actions

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const VERIFICATION_TOKEN = "EAACDZA59ohMoBABJdOkXYV0Q7MYE7ZA2U6eXbpCiOZBWytmh66xQ8Sg2yD8hcj61FtqQO4AnsFsZBRZCgXdE1a7eFKQ44v2OjCZC9JYXVbWhuosM5OGdEiZBT4FcdGfd9VZClBljY42ByWbiRxEH0y52RvPVeAo6c4JZBzJDVXcHQoAZDZD"
+const PAGE_ID = "245261069180348"
+
 
 app.set('port', (process.env.PORT || 3000))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -23,7 +25,6 @@ app.get('/webhook', function (req, res) {
     res.send('Error, wrong validation token');
   }
 });
-
 
 
 
@@ -53,7 +54,6 @@ app.post('/webhook/', function (req, res) {
       }
 
       else if (event.postback) {
-        send_text(sender, "Got a post back");
         let text = event.postback.payload;
 
         switch(text.toLowerCase()){
@@ -65,8 +65,12 @@ app.post('/webhook/', function (req, res) {
           send_today(sender);
           break;
 
+          case("yep"):
+          send_intro(sender);
+          break;
+
           default:
-          send_text(sender, "Text received, echo: " + text.substring(0, 200));
+          send_text(sender, text);
         }
       }
 
@@ -85,7 +89,7 @@ app.listen(app.get('port'), function() {
 //Sending messages
 
 function send_text(sender, text) {
-    let messageData = { text:text }
+    let messageData = { text:"Type 'play' to find games" }
 
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -202,9 +206,50 @@ function send_today(sender){
 }
 
 function send_tomorrow(sender){
-
+  send_today(sender);
 }
 
 function send_soon(sender){
+  send_today(sender);
+}
 
+
+function send_intro(sender) {
+    let messageData = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "How can I help you?",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "Play Football",
+              "payload": "play"
+            },
+            {
+              "type": "postback",
+              "title": "Find Players",
+              "payload": "find players"
+            }
+          ]
+        }
+      }
+    }
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:VERIFICATION_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
