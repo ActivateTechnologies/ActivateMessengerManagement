45a2cd9b8a416b5bc3bd66b510769ec755bc7e46
Anirudh <anirudhx5@gmail.com>
2016-06-13 11:58:23 +0100

    verification

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const VALIDATION_TOKEN = "EAACDZA59ohMoBABJdOkXYV0Q7MYE7ZA2U6eXbpCiOZBWytmh66xQ8Sg2yD8hcj61FtqQO4AnsFsZBRZCgXdE1a7eFKQ44v2OjCZC9JYXVbWhuosM5OGdEiZBT4FcdGfd9VZClBljY42ByWbiRxEH0y52RvPVeAo6c4JZBzJDVXcHQoAZDZD"
 
 app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
 
-// Index route
+
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send("Hi, I'm the Kickabout chat bot")
 })
 
 // for Facebook verification
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <YOUR_VERIFY_TOKEN>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
+
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
+    
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
@@ -38,10 +45,9 @@ app.post('/webhook/', function (req, res) {
         continue
       }
     }
-    res.sendStatus(200)
-  })
 
-const token = "EAACDZA59ohMoBABJdOkXYV0Q7MYE7ZA2U6eXbpCiOZBWytmh66xQ8Sg2yD8hcj61FtqQO4AnsFsZBRZCgXdE1a7eFKQ44v2OjCZC9JYXVbWhuosM5OGdEiZBT4FcdGfd9VZClBljY42ByWbiRxEH0y52RvPVeAo6c4JZBzJDVXcHQoAZDZD"
+    res.sendStatus(200)
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
