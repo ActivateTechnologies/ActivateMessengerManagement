91c92f1378696f0fef773f1c9185103e2eb79296
Anirudh <anirudhx5@gmail.com>
2016-06-14 10:00:47 +0100

    modules

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request')
 const app = express()
 const VERIFICATION_TOKEN = "EAACDZA59ohMoBABJdOkXYV0Q7MYE7ZA2U6eXbpCiOZBWytmh66xQ8Sg2yD8hcj61FtqQO4AnsFsZBRZCgXdE1a7eFKQ44v2OjCZC9JYXVbWhuosM5OGdEiZBT4FcdGfd9VZClBljY42ByWbiRxEH0y52RvPVeAo6c4JZBzJDVXcHQoAZDZD"
 const PAGE_ID = "245261069180348"
+const send = require('./reply_functions')
 
 
 app.set('port', (process.env.PORT || 3000))
@@ -41,19 +42,23 @@ app.post('/webhook/', function (req, res) {
 
         switch(text.toLowerCase()){
           case("play"):
-          send_play(sender);
+          send.play(sender);
           break;
 
           case("today"):
-          send_today(sender);
+          send.today(sender);
           break;
 
-          case("help"):
-          send_default(sender);
+          case("tomorrow"):
+          send.tomorrow(sender);
+          break;
+
+          case("soon"):
+          send.soon(sender);
           break;
 
           default:
-          send_text(sender, "Type 'play' to find games or 'help' for more options.");
+          send.default(sender);
         }
       }
 
@@ -62,19 +67,15 @@ app.post('/webhook/', function (req, res) {
 
         switch(text.toLowerCase()){
           case("play"):
-          send_play(sender);
+          send.play(sender);
           break;
 
           case("today"):
-          send_today(sender);
-          break;
-
-          case("yep"):
-          send_default(sender);
+          send.today(sender);
           break;
 
           default:
-          send_default(sender);
+          send.default(sender);
         }
       }
 
@@ -87,167 +88,3 @@ app.post('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
-
-
-
-//Sending messages
-
-function send_default(sender) {
-    let messageData = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "How can I help you?",
-          "buttons": [
-            {
-              "type": "postback",
-              "title": "Play Football",
-              "payload": "play"
-            }
-          ]
-        }
-      }
-    }
-
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:VERIFICATION_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function send_text(sender, text) {
-    let messageData = { text: text }
-
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:VERIFICATION_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function send_play(sender) {
-    let messageData = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "When do you want to play?",
-          "buttons": [
-            {
-              "type": "postback",
-              "title": "Today",
-              "payload": "Today"
-            },
-            {
-              "type": "postback",
-              "title": "Tomorrow",
-              "payload": "Today"
-            },
-            {
-              "type": "postback",
-              "title": "Soon",
-              "payload": "Today"
-            }
-          ]
-        }
-      }
-    }
-
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:VERIFICATION_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function send_today(sender){
-
-  send_text(sender, "Awesome, here are my options for today. Tap the card to get directions.");
-
-  let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [
-                    {
-                      "title": "13:00-1400, 5-Aside, Free",
-                      "subtitle": "Whitfield Pl, Kings Cross, London W1T 5JX",
-                      "image_url": "https://www.openplay.co.uk/uploads/Cv6mBb44YbRSpaSA-500x_.jpg",
-                      "buttons": [{
-                          "type": "postback",
-                          "title": "Book",
-                          "payload": "Book",
-                      }],
-                  },
-                  {
-                      "title": "16:00-17:30, 11-Aside, £5",
-                      "subtitle": "Corams Fields, 93 Guilford St, London WC1N 1DN",
-                      "image_url": "https://www.openplay.co.uk/uploads/356_538f7d4165ba1-500x_.jpg",
-                      "buttons": [{
-                          "type": "postback",
-                          "title": "Book",
-                          "payload": "Book",
-                      }],
-                  }
-                ]
-            }
-        }
-    }
-
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:VERIFICATION_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function send_tomorrow(sender){
-  send_today(sender);
-}
-
-function send_soon(sender){
-  send_today(sender);
-}
---
+++ reply_functions.js
@@ -0,0 +1,165 @@
+'use strict'
+
+module.exports = {
+
+  default(sender) {
+      let messageData = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "button",
+            "text": "How can I help you?",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Play Football",
+                "payload": "play"
+              }
+            ]
+          }
+        }
+      }
+
+      request({
+          url: 'https://graph.facebook.com/v2.6/me/messages',
+          qs: {access_token:VERIFICATION_TOKEN},
+          method: 'POST',
+          json: {
+              recipient: {id:sender},
+              message: messageData,
+          }
+      }, function(error, response, body) {
+          if (error) {
+              console.log('Error sending messages: ', error)
+          } else if (response.body.error) {
+              console.log('Error: ', response.body.error)
+          }
+      })
+  },
+
+  text(sender, text) {
+      let messageData = { text: text }
+
+      request({
+          url: 'https://graph.facebook.com/v2.6/me/messages',
+          qs: {access_token:VERIFICATION_TOKEN},
+          method: 'POST',
+          json: {
+              recipient: {id:sender},
+              message: messageData,
+          }
+      }, function(error, response, body) {
+          if (error) {
+              console.log('Error sending messages: ', error)
+          } else if (response.body.error) {
+              console.log('Error: ', response.body.error)
+          }
+      })
+  },
+
+  play(sender) {
+      let messageData = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "button",
+            "text": "When do you want to play?",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Today",
+                "payload": "Today"
+              },
+              {
+                "type": "postback",
+                "title": "Tomorrow",
+                "payload": "Today"
+              },
+              {
+                "type": "postback",
+                "title": "Soon",
+                "payload": "Today"
+              }
+            ]
+          }
+        }
+      }
+
+      request({
+          url: 'https://graph.facebook.com/v2.6/me/messages',
+          qs: {access_token:VERIFICATION_TOKEN},
+          method: 'POST',
+          json: {
+              recipient: {id:sender},
+              message: messageData,
+          }
+      }, function(error, response, body) {
+          if (error) {
+              console.log('Error sending messages: ', error)
+          } else if (response.body.error) {
+              console.log('Error: ', response.body.error)
+          }
+      })
+  },
+
+  today(sender){
+
+    send_text(sender, "Awesome, here are my options for today. Tap the card to get directions.");
+
+    let messageData = {
+          "attachment": {
+              "type": "template",
+              "payload": {
+                  "template_type": "generic",
+                  "elements": [
+                      {
+                        "title": "13:00-1400, 5-Aside, Free",
+                        "subtitle": "Whitfield Pl, Kings Cross, London W1T 5JX",
+                        "image_url": "https://www.openplay.co.uk/uploads/Cv6mBb44YbRSpaSA-500x_.jpg",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Book",
+                            "payload": "Book",
+                        }],
+                    },
+                    {
+                        "title": "16:00-17:30, 11-Aside, £5",
+                        "subtitle": "Corams Fields, 93 Guilford St, London WC1N 1DN",
+                        "image_url": "https://www.openplay.co.uk/uploads/356_538f7d4165ba1-500x_.jpg",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Book",
+                            "payload": "Book",
+                        }],
+                    }
+                  ]
+              }
+          }
+      }
+
+      request({
+          url: 'https://graph.facebook.com/v2.6/me/messages',
+          qs: {access_token:VERIFICATION_TOKEN},
+          method: 'POST',
+          json: {
+              recipient: {id:sender},
+              message: messageData,
+          }
+      }, function(error, response, body) {
+          if (error) {
+              console.log('Error sending messages: ', error)
+          } else if (response.body.error) {
+              console.log('Error: ', response.body.error)
+          }
+      })
+  },
+
+  tomorrow(sender){
+    send_today(sender);
+  },
+
+  soon(sender){
+    send_today(sender);
+  }
+
+}
