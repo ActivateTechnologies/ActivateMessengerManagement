b219aeff3969082dd0eadb8d2c79c8db146dd739
Anirudh <anirudhx5@gmail.com>
2016-06-14 11:17:47 +0100

    changing payload

--- index.js
+++ index.js
@@ -210,6 +210,7 @@ function send_today(sender){
   send_text(sender, "Awesome, here are my options for today. Tap the card to get directions.");
 
   address = "Corams Fields, 93 Guilford St, London WC1N 1DN"
+  payload = "Book" + "http://maps.google.com/?q=" + address
 
   let messageData = {
         "attachment": {
@@ -224,7 +225,7 @@ function send_today(sender){
                       "buttons": [{
                           "type": "postback",
                           "title": "Book",
-                          "payload": "Book"
+                          "payload": payload,
                       }],
                   },
                   {
@@ -234,7 +235,7 @@ function send_today(sender){
                       "buttons": [{
                           "type": "postback",
                           "title": "Book",
-                          "payload": "Book" + "http://maps.google.com/?q=" + address
+                          "payload": payload,
                       }],
                   }
                 ]
