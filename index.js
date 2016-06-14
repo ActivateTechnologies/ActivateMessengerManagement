22a6ced5581488c25a413db3c55c552c170ac6e7
Anirudh <anirudhx5@gmail.com>
2016-06-14 10:33:32 +0100

    final test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const VERIFICATION_TOKEN = "EAACDZA59ohMoBABJdOkXYV0Q7MYE7ZA2U6eXbpCiOZBWytmh66xQ8Sg2yD8hcj61FtqQO4AnsFsZBRZCgXdE1a7eFKQ44v2OjCZC9JYXVbWhuosM5OGdEiZBT4FcdGfd9VZClBljY42ByWbiRxEH0y52RvPVeAo6c4JZBzJDVXcHQoAZDZD"
 const PAGE_ID = "245261069180348"
 const send = require('./reply_functions')
 
@@ -65,8 +64,6 @@ app.post('/webhook/', function (req, res) {
       else if (event.postback) {
         let text = event.postback.payload;
 
-        sendTextMessage(sender, "postback");
-
         switch(text.toLowerCase()){
           case("play"):
           send.play(sender);
@@ -91,22 +88,3 @@ app.post('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 })
-
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: VERIFICATION_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
