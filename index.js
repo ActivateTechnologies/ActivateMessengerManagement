cacfffe95f0431e4768d81334331ea63631b4547
Anirudh <anirudhx5@gmail.com>
2016-06-14 11:26:11 +0100

    reverted send_today

--- index.js
+++ index.js
@@ -205,12 +205,64 @@ function send_play(sender) {
     })
 }
 
+// function send_today(sender){
+//
+//   send_text(sender, "Awesome, here are my options for today. Tap the card to get directions.");
+//
+//   address = "Corams Fields, 93 Guilford St, London WC1N 1DN";
+//   payload = "Book" + "http://maps.google.com/?q=";
+//
+//   let messageData = {
+//         "attachment": {
+//             "type": "template",
+//             "payload": {
+//                 "template_type": "generic",
+//                 "elements": [
+//                     {
+//                       "title": "13:00-1400, 5-Aside, Free",
+//                       "subtitle": "Whitfield Pl, Kings Cross, London W1T 5JX",
+//                       "image_url": "https://www.openplay.co.uk/uploads/Cv6mBb44YbRSpaSA-500x_.jpg",
+//                       "buttons": [{
+//                           "type": "postback",
+//                           "title": "Book",
+//                           "payload": "Book",
+//                       }],
+//                   },
+//                   {
+//                       "title": "16:00-17:30, 11-Aside, £5",
+//                       "subtitle": "Corams Fields, 93 Guilford St, London WC1N 1DN",
+//                       "image_url": "https://www.openplay.co.uk/uploads/356_538f7d4165ba1-500x_.jpg",
+//                       "buttons": [{
+//                           "type": "postback",
+//                           "title": "Book",
+//                           "payload": "Book",
+//                       }],
+//                   }
+//                 ]
+//             }
+//         }
+//     }
+//
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token:VERIFICATION_TOKEN},
+//         method: 'POST',
+//         json: {
+//             recipient: {id:sender},
+//             message: messageData,
+//         }
+//     }, function(error, response, body) {
+//         if (error) {
+//             console.log('Error sending messages: ', error)
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error)
+//         }
+//     })
+// }
+
 function send_today(sender){
 
   send_text(sender, "Awesome, here are my options for today. Tap the card to get directions.");
-
-  address = "Corams Fields, 93 Guilford St, London WC1N 1DN";
-  payload = "Book" + "http://maps.google.com/?q=";
 
   let messageData = {
         "attachment": {
