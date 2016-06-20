7549ef281499ea7b479f255b70f3cf35d36b6989
Anirudh <anirudhx5@gmail.com>
2016-06-20 11:26:33 +0100

    sending today_data for debugging

--- server/send.js
+++ server/send.js
@@ -112,7 +112,7 @@ function play(sender) {
 function cards(sender, today_data, day){
 
   text(sender, "Awesome, here are my options for " + day + ". Tap the card to get directions.");
-
+  text(sender, "at" + today_data)
   let messageData = today_data;
 
     request({
