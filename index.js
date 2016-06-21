0c2fd0dac9baf8e9b237de31c23e184d5310266e
Anirudh <anirudhx5@gmail.com>
2016-06-21 11:55:35 +0100

    separating games today, tomorrow, soon

--- index.js
+++ index.js
@@ -55,16 +55,39 @@ app.post('/input', function(req, res){
 });
 
 
-app.get('/games', function(req, res){
+app.get('/today', function(req, res){
   let now = new Date();
-  M.Game.find({when: {$gt: new Date(now.getFullYear(), now.getMonth(), now.getDate()-1)}}, function(err, result){
+  M.Game.find({when:{$gt: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1), $lt: new Date(now.getFullYear(), now.getMonth(), now.getDate()+1)}}, function(err, result){
     if(err){
       console.log(err);
     } else {
       res.send(result);
     }
   })
+})
 
+app.get('/tomorrow', function(req, res){
+  let now2 = new Date();
+  now2.setDate(now2.getDate()+1);
+  M.Game.find({when:{$gt: new Date(now2.getFullYear(), now2.getMonth(), now2.getDate() - 1), $lt: new Date(now2.getFullYear(), now2.getMonth(), now2.getDate()+1)}}, function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.get('/soon', function(req, res){
+  let now3 = new Date();
+  now3.setDate(now3.getDate()+2);
+  M.Game.find({when:{$gt: new Date(now3.getFullYear(), now3.getMonth(), now3.getDate() - 1)}}, function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  })
 })
 
 
