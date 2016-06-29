e1087ca953da3219fdff8d6850b94540b27dfe8d
Anirudh <anirudhx5@gmail.com>
2016-06-29 14:15:08 +0100

    shifted generator functions to send.js

--- index.js
+++ index.js
@@ -254,7 +254,7 @@ app.post('/webhook/', function (req, res) {
 
           send.directions(sender, address, latlong);
 
-          send.cards(sender, generate_card_for_booking(gameId, description))
+          send.cards(sender, send.generate_card_for_booking(gameId, description))
         }
 
         else {
@@ -283,7 +283,7 @@ app.post('/webhook/', function (req, res) {
                 today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc]);
               })
 
-              today_data = generate_card(today_data);
+              today_data = send.generate_card(today_data);
               send.cards(sender, today_data, "today");
 
               send.text(sender, result);
@@ -313,7 +313,7 @@ app.post('/webhook/', function (req, res) {
                 today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc]);
               })
 
-              today_data = generate_card(today_data);
+              today_data = send.generate_card(today_data);
               send.cards(sender, today_data, "today");
 
               send.text(sender, result);
@@ -343,7 +343,7 @@ app.post('/webhook/', function (req, res) {
                 today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc]);
               })
 
-              today_data = generate_card(today_data);
+              today_data = send.generate_card(today_data);
               send.cards(sender, today_data, "today");
 
               send.text(sender, result);
@@ -417,101 +417,3 @@ app.post('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'));
 })
-
-
-
-
-
-//////////// Data for sending
-
-function generate_card_element(name, address, image_url, latlong, gameId, attending, capacity, booked, description){
-
-  let pl = "More Info" + "|" + name + "|" + address + "|" + latlong + "|" + gameId + "|" + description;
-
-  let directions_link = "http://maps.google.com/?q=" + address;
-
-  if (attending > 0){
-    address = address + " (" + attending + " attending)";
-  }
-
-  if(attending == capacity || booked){
-    if(attending == capacity){
-      address = address + " (fully booked)";
-    }
-    if(booked){
-      address = address + " (You're going)";
-    }
-    let template = {
-      "title": name,
-      "subtitle": address,
-      "image_url": image_url,
-      "item_url": directions_link,
-    }
-    return template;
-  }
-  else {
-
-    let template = {
-      "title": name,
-      "subtitle": address,
-      "image_url": image_url,
-      "item_url": directions_link,
-      "buttons": [{
-          "type": "postback",
-          "title": "More Info",
-          "payload": pl,
-      }],
-    }
-
-    return template;
-  }
-}
-
-function generate_card_for_booking(gameId, description){
-
-  let pl = "Book" + "|" + gameId;
-
-  let template = {
-                    "attachment": {
-                      "type": "template",
-                      "payload": {
-                          "template_type": "button",
-                          "text": description,
-                          "buttons": [
-                              {
-                                "type": "postback",
-                                "title": "Book",
-                                "payload": pl,
-                              },
-                              {
-                                  "type": "postback",
-                                  "title": "No, thanks",
-                                  "payload": "No, thanks",
-                              }
-                            ],
-                          }
-                      }
-                  }
-
-  return template;
-}
-
-function generate_card(array){
-  let elements = [];
-  array.forEach(function(item){
-    //name, address, image_url, latlong, gameId, attending, capacity, booked
-    elements.push(generate_card_element(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8]));
-  });
-
-  var template = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": elements
-            }
-        }
-    }
-
-  return template;
-}
--- server/send.js
+++ server/send.js
@@ -115,7 +115,7 @@ function cards(sender, today_data, day){
     text(sender, "Awesome, here are my options for " + day + ". Tap the card to get directions.");
   }
 
-  let messageData = JSON.stringify(today_data);
+  let messageData = today_data;
 
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -179,10 +179,106 @@ function directions(sender, address, latlong){
   })
 }
 
+function generate_card_element(name, address, image_url, latlong, gameId, attending, capacity, booked, description){
+
+  let pl = "More Info" + "|" + name + "|" + address + "|" + latlong + "|" + gameId + "|" + description;
+
+  let directions_link = "http://maps.google.com/?q=" + address;
+
+  if (attending > 0){
+    address = address + " (" + attending + " attending)";
+  }
+
+  if(attending == capacity || booked){
+    if(attending == capacity){
+      address = address + " (fully booked)";
+    }
+    if(booked){
+      address = address + " (You're going)";
+    }
+    let template = {
+      "title": name,
+      "subtitle": address,
+      "image_url": image_url,
+      "item_url": directions_link,
+    }
+    return template;
+  }
+  else {
+
+    let template = {
+      "title": name,
+      "subtitle": address,
+      "image_url": image_url,
+      "item_url": directions_link,
+      "buttons": [{
+          "type": "postback",
+          "title": "More Info",
+          "payload": pl,
+      }],
+    }
+
+    return template;
+  }
+}
+
+function generate_card_for_booking(gameId, description){
+
+  let pl = "Book" + "|" + gameId;
+
+  let template = {
+                    "attachment": {
+                      "type": "template",
+                      "payload": {
+                          "template_type": "button",
+                          "text": description,
+                          "buttons": [
+                              {
+                                "type": "postback",
+                                "title": "Book",
+                                "payload": pl,
+                              },
+                              {
+                                  "type": "postback",
+                                  "title": "No, thanks",
+                                  "payload": "No, thanks",
+                              }
+                            ],
+                          }
+                      }
+                  }
+
+  return template;
+}
+
+function generate_card(array){
+  let elements = [];
+  array.forEach(function(item){
+    //name, address, image_url, latlong, gameId, attending, capacity, booked
+    elements.push(generate_card_element(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8]));
+  });
+
+  var template = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": elements
+            }
+        }
+    }
+
+  return template;
+}
+
+
 module.exports = {
   age: age,
   text: text,
   play: play,
   cards: cards,
-  directions: directions
+  directions: directions,
+  generate_card_element: generate_card_element,
+  generate_card: generate_card,
+  generate_card_for_booking: generate_card_for_booking
 }
