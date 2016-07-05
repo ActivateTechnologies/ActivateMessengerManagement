0576895c3cc29c608db55599c40e5fc03a8c933f
Anirudh <anirudhx5@gmail.com>
2016-07-05 15:41:01 +0100

    trying to fix the page subscription

--- index.js
+++ index.js
@@ -270,35 +270,9 @@ app.post('/webhook/', function (req, res) {
       let sender = event.sender.id
 
       if (event.message && event.message.text) {
-        // M.User.find({userId: sender}, function(err, result){
-        //   if(result[0].eligible){
-        //
-        //     let now = new Date();
-        //
-        //     M.Game.find({when:{$gt: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)}}, function(err, result){
-        //
-        //       let today_data = [];
-        //       result.forEach(function(item){
-        //         let booked = false;
-        //         let join = item.joined;
-        //
-        //         join.forEach(function(i){
-        //           if(i.userId === sender){
-        //             booked = true;
-        //           }
-        //         });
-        //         today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc, item.when]);
-        //       })
-        //
-        //       today_data = send.generate_card(today_data);
-        //       send.cards(sender, today_data, "today");
-        //     })
-        //
-        //   }
-        //   else {
-        //     send.text(sender, "Sorry, you're not old enough to play");
-        //   }
-        // })
+
+        //add check here to find if user exists
+
         send.text(sender, "Hi, there");
       }
 
@@ -363,91 +337,6 @@ app.post('/webhook/', function (req, res) {
 
         else {
           switch(text.toLowerCase()){
-
-            // case("today"):
-            //
-            // let now = new Date();
-            //
-            // M.Button.update({name:"Today"}, {$push: {activity: {userId:sender, time: now}}}, {upsert: true}, function(err){
-            //   console.log(err);
-            // })
-            //
-            // M.Game.find({when:{$gt: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1), $lt: new Date(now.getFullYear(), now.getMonth(), now.getDate()+1)}}, function(err, result){
-            //
-            //   let today_data = [];
-            //   result.forEach(function(item){
-            //     let booked = false;
-            //     let join = item.joined;
-            //
-            //     join.forEach(function(i){
-            //       if(i.userId === sender){
-            //         booked = true;
-            //       }
-            //     });
-            //     today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc, item.when]);
-            //   })
-            //
-            //   today_data = send.generate_card(today_data);
-            //   send.cards(sender, today_data, "today");
-            // })
-            // break;
-            //
-            // case("tomorrow"):
-            // let now2 = new Date();
-            // now2.setDate(now2.getDate()+1);
-            //
-            // M.Button.update({name:"Tomorrow"}, {$push: {activity: {userId:sender, time: new Date()}}}, {upsert: true}, function(err){
-            //   console.log(err);
-            // })
-            //
-            // M.Game.find({when:{$gt: new Date(now2.getFullYear(), now2.getMonth(), now2.getDate() - 1), $lt: new Date(now2.getFullYear(), now2.getMonth(), now2.getDate()+1)}}, function(err, result){
-            //
-            //   let today_data = [];
-            //   result.forEach(function(item){
-            //     let booked = false;
-            //     let join = item.joined;
-            //
-            //     join.forEach(function(i){
-            //       if(i.userId === sender){
-            //         booked = true;
-            //       }
-            //     });
-            //     today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc]);
-            //   })
-            //
-            //   today_data = send.generate_card(today_data);
-            //   send.cards(sender, today_data, "today");
-            // })
-            // break;
-            //
-            // case("soon"):
-            // let now3 = new Date();
-            // now3.setDate(now3.getDate()+2);
-            //
-            // M.Button.update({name:"Soon"}, {$push: {activity: {userId:sender, time: new Date()}}}, {upsert: true}, function(err){
-            //   console.log(err);
-            // })
-            //
-            // M.Game.find({when:{$gt: new Date(now3.getFullYear(), now3.getMonth(), now3.getDate() - 1)}}, function(err, result){
-            //
-            // let today_data = [];
-            // result.forEach(function(item){
-            //     let booked = false;
-            //     let join = item.joined;
-            //
-            //     join.forEach(function(i){
-            //       if(i.userId === sender){
-            //         booked = true;
-            //       }
-            //     });
-            //     today_data.push([item.name, item.address, item.image_url, item.latlong, item._id, item.joined.length, item.capacity, booked, item.desc]);
-            //   })
-            //
-            //   today_data = send.generate_card(today_data);
-            //   send.cards(sender, today_data, "today");
-            // })
-            // break;
-
             case('start'):
             send.start(sender);
             break;
@@ -457,12 +346,12 @@ app.post('/webhook/', function (req, res) {
             M.Button.update({name:"Yep"}, {$push: {activity: {userId:sender, time: new Date()}}}, {upsert: true}, function(err){
               console.log(err);
             })
+            send.text(sender, "some text");
             send.link(sender);
 
             break;
 
             default:
-            // send.play(sender);
             let now = new Date();
 
             M.Game.find({when:{$gt: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)}}, function(err, result){
