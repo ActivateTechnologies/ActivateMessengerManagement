c16a85a6fe94b6ce5cb61a04f36f947c251cda34
Anirudh <anirudhx5@gmail.com>
2016-07-14 15:16:46 +0100

    fixing the url

--- routes/game.js
+++ routes/game.js
@@ -5,7 +5,16 @@ const router = express.Router()
 const M = require('./../server/schemas.js')
 
 router.get('/game', function(req, res){
-  res.render('game', {gid: req.query.gid, gameName: req.query.gameName})
+  M.Game.find({_id:req.query.gid}, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    if(result.length > 0){
+      res.render('game', {gid: req.query.gid, gameName: result[0].name});
+    } else {
+      res.render('game', {gid: req.query.gid, gameName: "Kickabout Game"})
+    }
+  })
 })
 
 module.exports = router
