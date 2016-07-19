0d73c58e6e05eba5d46c27922dce5f4c968a7e63
Anirudh <anirudhx5@gmail.com>
2016-07-19 16:36:44 +0100

    loggin people reached

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -13,26 +13,22 @@ router.post('/broadcast', function(req, res){
   let type = req.query.typ;
   let message = req.query.message;
 
-  let reached = 0;
-
   if(type === "message"){
     M.User.find({}, function(err, result){
-      reached = result.length;
       result.forEach(function(item){
         send.text(item.userId, message);
       })
+      res.send("People reached: " + result.length)
     })
   }
   else if(type === "upcomingGames"){
     M.User.find({}, function(err, result){
-      reached = result.length;
       result.forEach(function(item){
         send.allGames(item.userId);
       })
+      res.send("People reached: " + result.length)
     })
   }
-
-  res.send("People reached: " + reached)
 })
 
 module.exports = router
