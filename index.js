c334742c8a8789b356b9cd4a408b3a0f8912bd4c
Anirudh <anirudhx5@gmail.com>
2016-07-19 10:16:03 +0100

    public link is working

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use(require('./routes/games'))
 app.use(require('./routes/webhook'))
 app.use(require('./routes/payment'))
 app.use(require('./routes/game'))
+app.use(require('./routes/check'))
 
 app.get('/', function(req, res){
   res.render('home')
