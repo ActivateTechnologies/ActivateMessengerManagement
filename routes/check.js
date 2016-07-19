7f2f089db098100e2344d0259a95c456cae05bd3
Anirudh <anirudhx5@gmail.com>
2016-07-19 15:37:33 +0100

    handling cors

--- routes/check.js
+++ routes/check.js
@@ -6,9 +6,24 @@ const M = require('./../server/schemas.js')
 
 router.get('/check', function(req, res){
   M.User.find({facebookID:req.query.fbid}, function(err, result){
+
     if(err){
       console.log(err);
     }
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     if(result.length > 0){
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({ check: true}));
