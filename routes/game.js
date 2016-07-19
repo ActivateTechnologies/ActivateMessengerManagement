189353c307aafb0c093f5c03de0f9d94566ba807
avinashjavaji <avinashjavaji@gmail.com>
2016-07-19 14:51:24 +0100

    Players going to game list

--- routes/game.js
+++ routes/game.js
@@ -15,6 +15,29 @@ router.get('/game', function(req, res){
       res.render('game', {gid: req.query.gid, gameName: "Kickabout Game"})
     }
   })
-})
+});
+
+router.get('/players', function(req, res){
+  M.Game.find({_id:req.query.gid}, function(err, games){
+    if(err){
+      console.log(err);
+      res.send([]);
+    }
+    else if (games[0].joined && games[0].joined.length) {
+      var playerIds = [];
+      games[0].joined.forEach(function(player, i) {
+        playerIds.push(player._id);
+      });
+      M.User.find({_id:{ $in : playerIds}}, function(err, users){
+        if(err){
+          console.log(err);
+        }
+        res.send(users)
+      })
+    } else {
+      res.send([]);
+    }
+  });
+});
 
 module.exports = router
