eec7c59c3f5fd0291737ff1d24e01d13367d263c
Anirudh <anirudhx5@gmail.com>
2016-07-19 15:49:15 +0100

    chaning cors for production

--- routes/check.js
+++ routes/check.js
@@ -12,7 +12,7 @@ router.get('/check', function(req, res){
     }
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://limitless-sierra-68694.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
