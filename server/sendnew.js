b27ebe4f39e5994cab0133c17c32f7f497dd80d3
avinashjavaji <avinashjavaji@gmail.com>
2016-07-20 15:09:40 +0100

    -

--- server/sendnew.js
+++ server/sendnew.js
@@ -19,13 +19,35 @@ function text(sender, text) {
       }
   }, function(error, response, body) {
       if (error) {
-          console.log('Error sending messages: ', error)
+          console.log('Error sending messages (to user ' 
+          	+ sender + '): ', error)
       } else if (response.body.error) {
-          console.log('Error: ', response.body.error)
+          console.log('Error sending messages (to user ' 
+          	+ sender + '): ', response.body.error);
       }
   })
 }
 
+function typingIndicator(sender, onOrOff) {
+  let typingStatus = (onOrOff) ? 'typing_on' : 'typing_off';
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:VERIFICATION_TOKEN},
+    method: 'POST',
+    json: {
+        recipient: {id:sender},
+        sender_action: typingStatus
+    }
+  }, function(error, response, body) {
+    if (error) {
+        console.log('Error setting typing indicator: ', error)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+    }
+  })
+}
+
 module.exports = {
-  text: text
+  text: text,
+  typingIndicator: typingIndicator
 }
