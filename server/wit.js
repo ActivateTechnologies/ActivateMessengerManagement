740b97f18fe3b97f2e1c040fee7e876d59be01f7
avinashjavaji <avinashjavaji@gmail.com>
2016-07-20 14:20:00 +0100

    -

--- server/wit.js
+++ server/wit.js
@@ -10,17 +10,6 @@ const VERIFICATION_TOKEN = config.VERIFICATION_TOKEN
 const Wit = require('node-wit').Wit;
 const log = require('node-wit').log;
 
-/*let Wit = null;
-let log = null;
-try {
-  // if running from repo
-  Wit = require('../').Wit;
-  log = require('../').log;
-} catch (e) {
-  Wit = require('node-wit').Wit;
-  log = require('node-wit').log;
-}*/
-
 // Wit actions
 const actions = {
   send(request, response) {
@@ -32,28 +21,6 @@ const actions = {
   	const quickreplies = response.quickreplies;
 
     console.log('actions.send() called');
-    /*// Our bot has something to say!
-	    // Let's retrieve the Facebook user whose session belongs to
-	    const recipientId = sessions[sessionId].fbid;
-	    if (recipientId) {
-	      // Yay, we found our recipient!
-	      // Let's forward our bot response to her.
-	      // We return a promise to let our bot know when we're done sending
-	      return fbMessage(recipientId, text)
-	      .then(() => null)
-	      .catch((err) => {
-	        console.error(
-	          'Oops! An error occurred while forwarding the response to',
-	          recipientId,
-	          ':',
-	          err.stack || err
-	        );
-	      });
-	    } else {
-	      console.error('Oops! Couldn\'t find user for session:', sessionId);
-	      // Giving the wheel back to our bot
-	      return Promise.resolve()
-	    }*/
   },
   countUpcomingGames(request/*, response*/) {
   	const sessionId = request.sessionId;
@@ -80,12 +47,11 @@ const wit = new Wit({
 });
 
 function sendConversationMessage(message, sender, context) {
+	console.log('Inside sendConversationMessage()');
 	// Setting up Wit bot
 	if (!context) {
 		context = {};
 	}
-	console.log('This is m:');
-	console.log(send);
 	//send.typingIndicator(sender, true);
 	wit.converse(sender, message, context)
 	.then((context) => {
