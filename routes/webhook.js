273aac11cebc915c0dc4ec06fcc241cd19aefe1e
avinashjavaji <avinashjavaji@gmail.com>
2016-07-20 15:44:55 +0100

    -

--- routes/webhook.js
+++ routes/webhook.js
@@ -23,58 +23,58 @@ router.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
 
     messaging_events.forEach(function(event){
-      if (!event.is_echo) {
-              let sender = event.sender.id;
-        if (event.optin) {
-          console.log("optin");
-          console.log(event.optin.ref);
-          send.publicLink(sender, event.optin.ref);
-        }
-        else if (event.message && event.message.text) {
-          M.User.find({userId: sender}, function(err, result){
-            if(result.length > 0){
-              send.processReceivedMessage(event.message.text, sender);
-              //send.allGames(sender);
-            }
-            else {
-              console.log('Going to call send.start()')
-              send.start(sender);
-            }
-          })
-        }
-        else if (event.postback) {
-          let text = event.postback.payload;
-
-          if(text.substring(0, 4) == "Book"){
-            send.book(sender, text);
+      let sender = event.sender.id;
+      if (event.optin) {
+        console.log("optin");
+        console.log(event.optin.ref);
+        send.publicLink(sender, event.optin.ref);
+      }
+      else if (event.message && event.message.text && !event.is_echo) {
+        //console.log('Got message: ' + event.message.text + ' from ' + sender);
+        M.User.find({userId: sender}, function(err, result){
+          if(result.length > 0){
+            send.processReceivedMessage(event.message.text, sender);
+            //send.allGames(sender);
           }
-
-          else if(text.substring(0, 6) == "Cancel"){
-            send.cancel_booking(sender, text);
-          }
-
-          else if(text.substring(0, 9) == "More Info"){
-            send.more_info(sender, text);
-          }
-
           else {
-            switch(text.toLowerCase()){
+            console.log('Going to call send.start()')
+            send.start(sender);
+          }
+        })
+      }
+      else if (event.postback) {
+        let text = event.postback.payload;
 
-              case('start'):
-              send.start(sender);
-              break;
+        if(text.substring(0, 4) == "Book"){
+          send.book(sender, text);
+        }
 
-              case("yep"):
-              send.yep(sender);
-              break;
+        else if(text.substring(0, 6) == "Cancel"){
+          send.cancel_booking(sender, text);
+        }
 
-              default:
-              send.allGames(sender);
+        else if(text.substring(0, 9) == "More Info"){
+          send.more_info(sender, text);
+        }
 
-            }
+        else {
+          switch(text.toLowerCase()){
+
+            case('start'):
+            send.start(sender);
+            break;
+
+            case("yep"):
+            send.yep(sender);
+            break;
+
+            default:
+            send.allGames(sender);
+
           }
         }
       }
+
     })
 
     res.sendStatus(200)
