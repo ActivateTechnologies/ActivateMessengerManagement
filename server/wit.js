8e1ce079f81c866256dea64221e43f2437e3cbc2
avinashjavaji <avinashjavaji@gmail.com>
2016-07-20 14:22:14 +0100

    -

--- server/wit.js
+++ server/wit.js
@@ -48,27 +48,40 @@ const wit = new Wit({
 
 function sendConversationMessage(message, sender, context) {
 	console.log('Inside sendConversationMessage()');
+	console.log(1);
 	// Setting up Wit bot
 	if (!context) {
 		context = {};
 	}
+	console.log(2);
 	//send.typingIndicator(sender, true);
 	wit.converse(sender, message, context)
 	.then((context) => {
+		console.log(3);
 	  console.log('Yay, got Wit.ai response: ' + JSON.stringify(context));
 	  if (context.type == 'msg' && context.msg) {
+	  	console.log(4);
       console.log('Sender is ' + sender);
+      console.log(5);
       send.text(sender, context.msg);
+      console.log(6);
       sendConversationMessage(message, sender, context);
+      console.log(7);
     } else if (context.type == 'action' && context.action) {
+    	console.log(8);
       if (context.action == 'countUpcomingGames') {
+      	console.log(9);
         console.log('Context action with countUpcomingGames');
         context.numUpcomingGames = 4;
+        console.log(10);
         sendConversationMessage(message, sender, context);
+        console.log(11);
       }
     } else if (context.type == 'stop') {
+    	console.log(12);
 	  	//send.typingIndicator(sender, false);
 	  }
+	  console.log(13);
 	})
 	.catch((error) => {
 		console.error(error);
