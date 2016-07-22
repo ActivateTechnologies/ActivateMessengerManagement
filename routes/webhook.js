b80ee198477cfe5cb6d4816a1517dcb7d3788bb3
Anirudh <anirudhx5@gmail.com>
2016-07-22 11:57:41 +0100

    trying to log

--- routes/webhook.js
+++ routes/webhook.js
@@ -23,7 +23,7 @@ router.post('/webhook/', function (req, res) {
 
   messaging_events.forEach(function(event){
 
-    console.log('Got event: ' + JSON.parse(event));
+    console.log('Got event: ' + event);
     let sender = event.sender.id;
     if (event.optin) {
       console.log("optin");
@@ -33,7 +33,7 @@ router.post('/webhook/', function (req, res) {
     else if (event.message && event.message.text && !event.message.is_echo) {
 
       send.processReceivedMessage(sender, event.message.text);
-      
+
       /*if (event.message.quick_reply) {
         console.log("quick_reply");
         console.log(event.message);
