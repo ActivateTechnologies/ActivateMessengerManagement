d21a795209ddedaa904efc58a849ad4e97d047a1
Anirudh <anirudhx5@gmail.com>
2016-07-22 12:12:47 +0100

    removed log

--- routes/webhook.js
+++ routes/webhook.js
@@ -23,7 +23,6 @@ router.post('/webhook/', function (req, res) {
 
   messaging_events.forEach(function(event){
 
-    console.log('Got something: ' + JSON.stringify(event));
     let sender = event.sender.id;
     if (event.optin) {
       console.log("optin");
