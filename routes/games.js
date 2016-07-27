baee252ed7ca63598dc3ceb5d173a66b8e8f2503
Anirudh <anirudhx5@gmail.com>
2016-07-27 13:59:10 +0100

    importing request

--- routes/games.js
+++ routes/games.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
+const request = require('request')
 const multer = require('multer')
 const fs = require('fs')
 const AWS = require('aws-sdk');
