6910cd371ef599153311dd626a556704fe8eac83
Anirudh <anirudhx5@gmail.com>
2016-07-27 15:50:35 +0100

    somehow it worked

--- routes/webhook.js
+++ routes/webhook.js
@@ -73,30 +73,30 @@ router.post('/webhook/', function (req, res) {
 
       else {
         if (config.DEVELOPMENT_STATUS == 'test') {
-          // M.User.find({userId: sender}, function(err, result){
-          //   if(result.length > 0){
-          //     // send.processReceivedMessage(sender, event.message.text);
-          //     send.allGames(sender);
-          //   }
-          //   else {
-          //     send.start(sender);
-          //   }
-          // })
-          send.processReceivedMessage(sender, event.message.text, () => {
-            //LUIS Did not find anything, so default response
-            console.log('Got message: ' + event.message.text + ' from ' + sender);
-            sendNew.text(sender, "I didn't quite understand that sorry. "
-             + "Here are all upcoming games. Alternatively, just say 'help'"
-             + " if you wanna talk to our support team", () => {
-              M.User.find({userId: sender}, function(err, result){
-                if (result.length > 0){
-                  //send.allGames(sender);
-                } else {
-                  send.start(sender);
-                }
-              })
-            });
-          });
+          M.User.find({userId: sender}, function(err, result){
+            if(result.length > 0){
+              // send.processReceivedMessage(sender, event.message.text);
+              send.allGames(sender);
+            }
+            else {
+              send.start(sender);
+            }
+          })
+          // send.processReceivedMessage(sender, event.message.text, () => {
+          //   //LUIS Did not find anything, so default response
+          //   console.log('Got message: ' + event.message.text + ' from ' + sender);
+          //   sendNew.text(sender, "I didn't quite understand that sorry. "
+          //    + "Here are all upcoming games. Alternatively, just say 'help'"
+          //    + " if you wanna talk to our support team", () => {
+          //     M.User.find({userId: sender}, function(err, result){
+          //       if (result.length > 0){
+          //         //send.allGames(sender);
+          //       } else {
+          //         send.start(sender);
+          //       }
+          //     })
+          //   });
+          // });
         }
 
         else {
