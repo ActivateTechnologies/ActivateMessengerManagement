ed55efded943f9a2d9be18724f18d70f8ce98b5a
Anirudh <anirudhx5@gmail.com>
2016-07-28 12:38:58 +0100

    still testing

--- routes/webhook.js
+++ routes/webhook.js
@@ -77,35 +77,34 @@ router.post('/webhook/', function (req, res) {
 
       // if message
       else {
-        if (config.DEVELOPMENT_STATUS == 'test') {
-          send.processReceivedMessage(sender, event.message.text, () => {
-            //LUIS Did not find anything, so default response
-            console.log('Got message: ' + event.message.text + ' from ' + sender);
-            sendNew.text(sender, "I didn't quite understand that sorry. "
-             + "Here are all upcoming games. Alternatively, just say 'help'"
-             + " if you wanna talk to our support team", () => {
-              M.User.find({userId: sender}, function(err, result){
-                if(result.length > 0 && result[0].facebookID){
-                  send.allGames(sender);
-                }
-                else {
-                  send.start(sender);
-                }
-              })
-            });
-          });
-        }
+        // if (config.DEVELOPMENT_STATUS == 'test') {
+        //   send.processReceivedMessage(sender, event.message.text, () => {
+        //     //LUIS Did not find anything, so default response
+        //     console.log('Got message: ' + event.message.text + ' from ' + sender);
+        //     sendNew.text(sender, "I didn't quite understand that sorry. "
+        //      + "Here are all upcoming games. Alternatively, just say 'help'"
+        //      + " if you wanna talk to our support team", () => {
+        //       M.User.find({userId: sender}, function(err, result){
+        //         if(result.length > 0 && result[0].facebookID){
+        //           send.allGames(sender);
+        //         }
+        //         else {
+        //           send.start(sender);
+        //         }
+        //       })
+        //     });
+        //   });
+        // }
 
-        else {
-          M.User.find({userId: sender}, function(err, result){
-            if(result.length > 0 && result[0].facebookID){
-              send.allGames(sender);
-            }
-            else {
-              send.start(sender);
-            }
-          })
-        }
+        M.User.find({userId: sender}, function(err, result){
+          if(result.length > 0 && result[0].facebookID){
+            console.log("has fbid");
+            send.allGames(sender);
+          }
+          else {
+            send.start(sender);
+          }
+        })
       }
     }
 
