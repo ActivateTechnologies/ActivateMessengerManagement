956d1d456ac37f55e20b547d44ac8328b098997e
Anirudh <anirudhx5@gmail.com>
2016-09-02 11:40:08 +0100

    added route for getting user data in csv

--- app/users/router.js
+++ app/users/router.js
@@ -4,7 +4,30 @@ const express = require('express');
 const router = express.Router();
 const S = require('./../strings');
 const M = require('./../schemas');
+const _ = require('underscore')
 
+/*
+  returns json data about users which can be imported in excel for querying*/
+router.get('/userdata', (req, res) => {
+  M.User.find({}, (err, users) => {
+    if(err) res.send(err);
+    let data = "Messenger Id Id,Name,Sign Up Date, Events Attended";
+    _.each(users, (user)=>{
+      data += "\r\n";
+      data += user.mid + ","
+            + (user.firstName + " " + user.lastName) + ","
+            + user.signedUpDate + ","
+            + user.events.length
+    })
+    res.setHeader('Content-disposition',
+      'attachment; filename=usersData.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(data);
+  });
+});
+
+/*
+  this returns data for /users route */
 router.get('/userAnalyticsData', (req, res) => {
   M.User.find({}, (err, users) => {
     res.json({'users': users})
