31aebfa4bdac8519c700db1b70452cd069db0059
Anirudh <anirudhx5@gmail.com>
2016-09-15 09:47:22 +0100

    added commands for menu and thread

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const status = 'production'; //'production' or 'ani'
+const status = 'production'; //'production' or 'test'
 
 // PRODUCTION | Kickabout | heroku git:remote -a limitless-sierra-68694
 // TEST | Monty Messenger | heroku git:remote -a kickabouttest
@@ -64,3 +64,40 @@ module.exports = function(code){
 	}
 
 }
+
+
+// For setting up Persistent Menu
+
+// curl -X POST -H "Content-Type: application/json" -d '{
+//   "setting_type" : "call_to_actions",
+//   "thread_state" : "existing_thread",
+//   "call_to_actions":[
+//     {
+//       "type":"postback",
+//       "title":"Show Events",
+//       "payload":"play"
+//     },
+//     {
+//       "type":"postback",
+//       "title":"My Events",
+//       "payload":"my events"
+//     },
+// 		{
+//       "type":"postback",
+//       "title":"Notifications",
+//       "payload":"notifications"
+//     }
+//   ]
+// }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAIL6dc0iLsBABYhZB3KsNv8N3uARJ7mqLDHPdSL2bEAqms3uuGRQMy4jXv9ZB2vLTIbR2xS3IuFZBatM20NVYtQyjKH3wZAMWzd2pRnB3tYHXHzwgxPMfdIiYSZBlhK9ZAOZBBtaSSeefdgjHTTDc3HaTzdB29sWY3f1KNyNF5nAZDZD
+
+// For setting up Get Started Button
+
+// curl -X POST -H "Content-Type: application/json" -d '{
+//   "setting_type":"call_to_actions",
+//   "thread_state":"new_thread",
+//   "call_to_actions":[
+//     {
+//       "payload":"yep"
+//     }
+//   ]
+// }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAQmEkmfekUBAD8bq0HX9x88drq8YxBOmGSGCynWJaQK6yZA28cuURx54GjgZBm5NALa3mvfxNFrJYingqlYgkSfLNUPyINyZBCoE2E8PkzsZAThe4zMKgS13llcP1mkItozytvmvyVOOfoKZBJXvURBDpowRYZCTsGkgJQYTQNgZDZD"
