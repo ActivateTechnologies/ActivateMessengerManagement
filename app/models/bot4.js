14a10f5f2167859cdaf16d762c373943c3289169
Anirudh <anirudhx5@gmail.com>
2016-09-17 13:09:29 +0100

    changed db

--- app/models/bot4.js
+++ app/models/bot4.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const mongoose = require('mongoose')
-let db = mongoose.createConnection("mongodb://anirudh:kickabout@ds011379.mlab.com:11379/uclfootball");
+let db = mongoose.createConnection("mongodb://anirudh:kickabout@ds051575.mlab.com:51575/bottest");
 
 module.exports = require('./schemas')(db)
