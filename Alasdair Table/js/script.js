a8d7ee4fd2bd7e276522ed0d89fbf8ee95290c0c
Anirudh <anirudhx5@gmail.com>
2016-09-20 14:18:52 +0100

    new table

--- Alasdair Table/js/script.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('sortTable' , []);
-
-app.controller('usersCtrl', ['$scope', '$http' , function($scope, $http) {
-	$http.get('./users.json').success(function(response) {
-		$scope.users = response.users;
-		console.log($scope.users);
-	})
-}])
