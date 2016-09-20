aa3529c45b4c893f212474a987d3251e95e2db13
Anirudh <anirudhx5@gmail.com>
2016-09-20 11:03:41 +0100

    updating front end

--- app/models/kickabout.js
+++ app/models/kickabout.js
@@ -1,6 +1,102 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 let db = mongoose.createConnection("mongodb://anirudh:kickabout@ds013664.mlab.com:13664/bot");
 
-module.exports = require('./schemas')(db)
+let schema = function(connection){
+
+  let userSchema = new Schema({
+    mid: String,
+    firstName: String,
+    lastName: String,
+    profilePic: String,
+    locale: String,
+    gender: String,
+    notifications: String,
+    signedUpDate: Date,
+    extras: Array,
+    events: Array,
+    conversationLocation: {
+      conversationName: String,
+      nodeId: String,
+      nodeType: String,
+      userErrorText: String
+    }
+  })
+
+  // extras is an object {}
+  // so any new property like striker can be added
+  // user.extras['position'] = "striker"
+
+  let eventSchema = new Schema({
+    name: String,
+    strapline: String,
+    image_name: String,
+    image_url: String,
+    latlong: String,
+    price: Number,
+    when: Date,
+    desc: String,
+    joined: [{
+      uid: String,
+      joinDate: Date
+    }],
+    capacity: Number
+  })
+
+  let analyticsSchema = new Schema({
+    name: String,
+    total: Number,
+    activity: [{
+      uid: String,
+      time: Date,
+      gid: String,
+      amount: Number
+    }]
+  })
+
+  let conversationsSchema = new Schema({
+    name: String,
+    next: Array
+  })
+
+  let registerSchema = new Schema({
+    name: String,
+    phoneNumber: String,
+    email: String,
+    fbPageURL: String,
+    description: String
+  })
+
+  if (!connection.models.User) {
+    console.log("user");
+    connection.model('User', userSchema);
+  }
+  if (!connection.models.Event) {
+    console.log("event");
+    connection.model('Event', eventSchema);
+  }
+  if (!connection.models.Analytics) {
+    console.log("analytics");
+    connection.model('Analytics', analyticsSchema)
+  }
+  if (!connection.models.Conversation) {
+    console.log("convo");
+    connection.model('Conversations', conversationsSchema)
+  }
+  if (!connection.models.Register) {
+    console.log("register");
+    connection.model('Register', registerSchema)
+  }
+
+  return {
+    User: connection.models.User,
+    Event: connection.models.Event,
+    Analytics: connection.models.Analytics,
+    Conversations: connection.models.Conversations
+    Register: connection.models.Register
+  };
+}
+
+module.exports = schema(db)
--- app/site/router.js
+++ app/site/router.js
@@ -7,6 +7,27 @@ router.get('/', function(req, res){
   res.render('site/home')
 })
 
+
+router.post('/register', function(req, res){
+  
+  const M = require('./../models/kickabout');
+
+  let register = M.Register({
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    fbPageURL: req.body.fbPageURL,
+    desc: req.body.desc
+  })
+
+  register.save((err)=>{
+    if (err) console.log(err);
+    res.send("Thanks, we'll be in touch soon")
+  })
+
+})
+
+
 router.get('/policy', function(req, res){
   res.render('site/policy');
 })
