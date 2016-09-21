433f0682ff22bb36a39a061a4c9b3caaf8e3f250
Anirudh <anirudhx5@gmail.com>
2016-09-21 17:20:56 +0100

    liverpool convo

--- app/users/router.js
+++ app/users/router.js
@@ -20,37 +20,37 @@ router.get('/userdata.:code', (req, res) => {
       data += (user.firstName + " " + user.lastName) + ","
             + user.gender + ","
       try {
-        data += (user.extras[1].phoneNumber + ',')
+        data += (user.phoneNumber + ',')
       }
       catch (e) {
         data += "NA" + ','
       }
       try {
-        data += (user.extras[2].email + ',')
+        data += (user.email + ',')
       }
       catch (e) {
         data += ("NA" + ',')
       }
       try {
-        data += (user.extras[3].preferredPosition + ',')
+        data += (user.preferredPosition + ',')
       }
       catch (e) {
         data += ("NA" + ',')
       }
       try {
-        data += (user.extras[4].backup + ',')
+        data += (user.backup + ',')
       }
       catch (e) {
         data += ("NA" + ',')
       }
       try {
-        data += (user.extras[5].level + ',')
+        data += (user.level + ',')
       }
       catch (e) {
         data += ("NA" + ',')
       }
       try {
-        data += ((user.extras[6])['type'] + ',')
+        data += (user['type'] + ',')
       }
       catch (e) {
         data += ("NA" + ',')
