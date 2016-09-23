1e410859b70c7ce6400748bedc35806e57d89318
Anirudh <anirudhx5@gmail.com>
2016-09-23 11:51:10 +0100

    redoing analytics

--- app/dashboard/router.js
+++ app/dashboard/router.js
@@ -4,32 +4,145 @@ const express = require('express');
 const router = express.Router();
 
 
+/*
+  Searchs Analytics collection for document with name=nameOfdocument,
+  returning bucketed analytics in the format:
+  {daysArray: array[7], weeksArray: array[12], monthsArray: array[12]}*/
+function processAnalyticsDataOverTime(M, nameOfdocument, callback) {
+
+  let now = new Date();
+  let aYearAgo = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate());
+
+  M.Analytics.findOne(
+   {name:nameOfdocument},
+   (err, result) => {
+    if (err) console.log(err);
+
+    if(result){
+
+      let dayMs = 86400 * 1000;
+      let activity = result.activity
+      let noOfDays = 367 + now.getDate();
+      let count = 0;
+      let startDate = new Date(aYearAgo.getTime() - now.getDate() * dayMs);
+      let daysArray = [], weeksArray = [], monthsArray = [];
+
+      //Create daysArray
+      for (let i = 0; i < noOfDays; i++) {
+        activity.forEach((item) => {
+          if (item.time > new Date(startDate.getTime() + i * dayMs) &&
+              item.time < new Date(startDate.getTime() + (i + 1) * dayMs)) {
+            count ++;
+          }
+        });
+        daysArray.push(count);
+        count = 0;
+      }
+
+      //Create weeksArray
+      count = 0;
+      startDate = new Date(now.getTime() - 12 * 7 * dayMs - dayMs);
+      let startDayIndex = noOfDays - (12 * 7 + now.getDay());
+      for (let i = startDayIndex; i < daysArray.length; i++) {
+        count += daysArray[i];
+        if ((i - startDayIndex) % 7 == 6) {
+          weeksArray.push(count);
+          count = 0;
+        }
+      }
+      weeksArray.push(count); //this week
+
+      //Create monthArray
+      let dateOfMonth = 0;
+      let monthDayCount = ((now.getMonth() > 1 && (now.getFullYear() % 4 == 0))
+        || (aYearAgo.getFullYear() % 4 == 0))
+        ? [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] //Leap year
+        : [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; //Non leap year
+      count = 0;
+      for (let i = 0; i < noOfDays; i++) {
+        dateOfMonth++;
+        count += daysArray[i];
+        if (dateOfMonth
+         == monthDayCount[(now.getMonth() + monthsArray.length) % 12]) {
+          dateOfMonth = 0;
+          monthsArray.push(count);
+          count = 0;
+        }
+      }
+      monthsArray.push(count);
+      let daysArrayModified = [];
+      let day = now.getDay();
+      for (let i = 0; i < 7; i++) {
+        if (i <= day) {
+          daysArrayModified.push(daysArray[daysArray.length - day + i]);
+        } else {
+          daysArrayModified.push(0);
+        }
+      }
+      callback({
+        daysArray: daysArrayModified,
+        weeksArray: weeksArray.slice(-12),
+        monthsArray: monthsArray.slice(-12)
+      });
+
+    }
+  })
+
+}
+
+
+/*
+  Renders the dashboard page and provides the main data:
+  { totalNoOfMembers:number, totalRevenue: number, totalNoOfTickets: number}*/
 router.get('/dashboard.:code', isLoggedIn, (req, res) => {
 
   const code = req.params.code;
   const S = require('./../strings')(code);
   const Analytics = require('./analytics.js')(code);
+  const M = require('./../models/' + code);
 
-  Analytics.getDashboardStats((data) => {
-    res.render('dashboard/dashboard', {
-      totalNoOfMembers: data.totalNoOfMembers,
-      totalRevenue: data.totalRevenue.toFixed(2),
-      totalNoOfTickets: data.totalNoOfTickets,
-      s: {company: S.s.company}
-    });
+  M.User.count((err, count) => {
+    if (err) console.log(err);
+    else {
+      M.Analytics.findOne({name:"Payments"}, (err, result) => {
+        if (err) console.log(err);
+
+        if (result) {
+          res.render('dashboard/dashboard', {
+            totalNoOfMembers: count,
+            totalRevenue: result.total,
+            totalNoOfTickets: result.activity.length,
+            s: {company: S.s.company}
+          });
+        }
+
+        else {
+          res.render('dashboard/dashboard', {
+            totalNoOfMembers: count,
+            totalRevenue: 0,
+            totalNoOfTickets: 0,
+            s: {company: S.s.company}
+          });
+        }
+      });
+    }
   });
 
 });
 
+
+/* send all the data to generate the graphs */
 router.get('/dashboardData.:code', (req, res) => {
 
   const code = req.params.code;
   const S = require('./../strings')(code);
   const Analytics = require('./analytics.js')(code);
+  const M = require('./../models/' + code);
 
   let requiredData = req.query.requiredData;
+  
   if (requiredData == 'getTicketsSoldOverTime') {
-    Analytics.getTicketsSoldOverTime((data, error) => {
+    processAnalyticsDataOverTime(M, "Payments", (data, error) => {
       if (error) {
         console.log(error);
         res.send('Error');
@@ -37,22 +150,11 @@ router.get('/dashboardData.:code', (req, res) => {
       else {
         res.send(data);
       }
-    })
+    });
   }
 
   else if (requiredData == 'getNewMembersOverTime') {
-    Analytics.getNewMembersOverTime((data, error) => {
-      if (error) {
-        console.log(error);
-        res.send('Error');
-      } else {
-        res.send(data);
-      }
-    })
-  }
-
-  else if (requiredData == 'getButtonHitsOverTime') {
-    Analytics.getButtonHitsOverTime((data, error) => {
+    processAnalyticsDataOverTime(M, "NewUsers", (data, error) => {
       if (error) {
         console.log(error);
         res.send('Error');
@@ -60,7 +162,19 @@ router.get('/dashboardData.:code', (req, res) => {
       else {
         res.send(data);
       }
-    })
+    });
+  }
+
+  else if (requiredData == 'getButtonHitsOverTime') {
+    processAnalyticsDataOverTime(M, "Button:More Info", (data, error) => {
+      if (error) {
+        console.log(error);
+        res.send('Error');
+      }
+      else {
+        res.send(data);
+      }
+    });
   }
 
 });
