e577b847c50a1c450d629cc65e54e8ccd96b4204
Alasdair Hall <al@dhsound.co.uk>
2016-09-30 11:45:15 +0100

    Created custom orderBy pipe

---
+++ public/js/angular/ts/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { User }           from './user';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+
+export class UserService {
+  private userUrl = '/userAnalyticsData.<%= s.company.code %>';  // URL to web API
+
+  constructor (private http: Http) {}
+
+  getUsers (): Observable<User[]> {
+    return this.http.get(this.userUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.users || { };
+  }
+  
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
---
+++ public/js/angular/ts/user.ts
@@ -0,0 +1,15 @@
+export class User {
+  profilePic: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  phoneNumber: string;
+  email: string;
+  preferredPosition: string;
+  backupPosition: string;
+  level: string;
+  type: string;
+  signedUpDate: string;
+  interactionTime: string;
+  receivedTime: string;
+}
