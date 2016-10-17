ad5909372489757f51eeaf01f02f2ba5afac95bf
Anirudh <anirudhx5@gmail.com>
2016-10-17 11:14:01 +0100

    adding models

---
+++ app/models/birmingham.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+let db = mongoose.createConnection("mongodb://anirudh:kickabout@ds057806.mlab.com:57806/birmingham");
+
+
+let schema = function(connection){
+
+  let userSchema = new Schema({
+    mid: String,
+    firstName: String,
+    lastName: String,
+    profilePic: String,
+    locale: String,
+    gender: String,
+    notifications: String,
+    signedUpDate: Date,
+    interactionTime: Date,
+    receivedTime: Date,
+
+    phoneNumber: String,
+    email: String,
+    preferredPosition: String,
+    ethnicity: String,
+    level: String,
+
+    events: Array,
+    conversationLocation: {
+      conversationName: String,
+      nodeId: String,
+      nodeType: String,
+      userErrorText: String
+    }
+  })
+
+  let eventSchema = new Schema({
+    name: String,
+    strapline: String,
+    image_name: String,
+    image_url: String,
+    latlong: String,
+    price: Number,
+    when: Date,
+    desc: String,
+    joined: [{
+      uid: String,
+      joinDate: Date
+    }],
+    capacity: Number
+  })
+
+  let analyticsSchema = new Schema({
+    name: String,
+    total: Number,
+    activity: [{
+      uid: String,
+      time: Date,
+      gid: String,
+      amount: Number
+    }]
+  })
+
+  let conversationsSchema = new Schema({
+    name: String,
+    next: Array
+  })
+
+
+  if (!connection.models.User) {
+    console.log("user");
+    connection.model('User', userSchema);
+  }
+  if (!connection.models.Event) {
+    console.log("event");
+    connection.model('Event', eventSchema);
+  }
+  if (!connection.models.Analytics) {
+    console.log("analytics");
+    connection.model('Analytics', analyticsSchema)
+  }
+  if (!connection.models.Conversation) {
+    console.log("convo");
+    connection.model('Conversations', conversationsSchema)
+  }
+
+  return {
+    User: connection.models.User,
+    Event: connection.models.Event,
+    Analytics: connection.models.Analytics,
+    Conversations: connection.models.Conversations
+  };
+}
+
+module.exports = schema(db)
